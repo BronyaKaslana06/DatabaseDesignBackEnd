acf46a2ae317536e52f800973b1fa5d2c8aa5128
Ayalyt <110286491+Ayalyt@users.noreply.github.com>
2023-08-24 15:08:42 +0800

    数据库映射记录 2023.8.24 15:00

---
+++ BSS_EFCore/Migrations/20230824070307_init123.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSS_EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class init123 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "city",
+                schema: "C##CAR",
+                table: "SWITCH_STATION",
+                newName: "City");
+
+            migrationBuilder.AddColumn<float>(
+                name: "ParkingFee",
+                schema: "C##CAR",
+                table: "SWITCH_STATION",
+                type: "BINARY_FLOAT",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                schema: "C##CAR",
+                table: "SWITCH_STATION",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeSpan",
+                schema: "C##CAR",
+                table: "SWITCH_STATION",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParkingFee",
+                schema: "C##CAR",
+                table: "SWITCH_STATION");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                schema: "C##CAR",
+                table: "SWITCH_STATION");
+
+            migrationBuilder.DropColumn(
+                name: "TimeSpan",
+                schema: "C##CAR",
+                table: "SWITCH_STATION");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                schema: "C##CAR",
+                table: "SWITCH_STATION",
+                newName: "city");
+        }
+    }
+}
